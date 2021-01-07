c0711b2878de100adb52e0bed74b6c3c05556fb0
M. Arda ÇAKICI <62748759+ardacakici@users.noreply.github.com>
2021-01-07 13:17:20 +0300

    Entities...

---
+++ CarGallery.Entites/Abstract/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGallery.Entites.Abstract
+{
+    public interface IEntity
+    {
+    }
+}
---
+++ CarGallery.Entites/Concrate/Car.cs
@@ -0,0 +1,28 @@
+﻿using CarGallery.Entites.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGallery.Entites.Concrate
+{
+    public class Car:IEntity
+    {
+        public int CarId { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public Car()
+        {
+
+        }
+
+        public Car(string model, decimal price)
+        {
+            Model = model;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"{CarId,-5}{Model,-30}{Price,-30}";
+        }
+    }
+}
