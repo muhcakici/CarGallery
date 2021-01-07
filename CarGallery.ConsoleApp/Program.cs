0e98695f40c1d547d999109b0cdff3a09445712c
M. Arda ÇAKICI <62748759+ardacakici@users.noreply.github.com>
2021-01-07 14:41:25 +0300

    ConsoleApp

---
+++ CarGallery.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using CarGallery.Business.Abstract;
+using CarGallery.Business.CrossCuttingConcern.DependencyRecorvers.Ninject;
+using System;
+
+namespace CarGallery.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carService = InstanceFactory.Get<ICarService>();
+            var list = carService.GetAll();
+            foreach (var c in list)
+            {
+                Console.WriteLine(c);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- CarGalleryDataAcsess/Concrate/EntityFramework/CarGalleryContext.cs
+++ CarGalleryDataAcsess/Concrate/EntityFramework/CarGalleryContext.cs
@@ -11,7 +11,7 @@ namespace CarGalleryDataAcsess.Concrate.EntityFramework
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
-              .UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LibraryDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+              .UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CarDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
         public DbSet<Car> Cars { get; set; }
     }
